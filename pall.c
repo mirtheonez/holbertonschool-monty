ea6103c4fad30fe7f0cad745fe52d2441216e83c
nourbenhazem <claudiabenhazem@gmail.com>
2023-06-08 13:31:52 +0100

    mont

--- pall.c
+++ pall.c
@@ -1,19 +1,19 @@
 #include "monty.h"
 
 /**
-* _pall - Prints all the values on the stack\
-*		starting from the top of the stack
-*
-* @stack: Double pointer to the top of the stack
-* @line_number: The line number where the pall opcode is located
-* Return: void
-*/
+ * _pall - Prints all the values on the stack\
+ *		starting from the top of the stack
+ *
+ * @stack: Double pointer to the top of the stack
+ * @line_number: The line number where the pall opcode is located
+ * Return: void
+ */
 void _pall(stack_t **stack, unsigned int line_number)
 {
 	(void)line_number;
-	stack_t *node;
+	stack_t *node
 
-	node = *stack;
+		= *stack;
 	while (node)
 	{
 		printf("%d\n", node->n);
